4bf86393e5766c29acca78276452ce60d2bd83d5
Taeyoon Kim <bbung9704@yonsei.ac.kr>
2021-01-05 16:26:37 +0900

    Profile 모델 ImageField 추가

--- account/models.py
+++ account/models.py
@@ -4,3 +4,4 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nickname = models.CharField(max_length=16, blank=False)
+    image = models.ImageField(blank=True)
