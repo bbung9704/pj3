485f8ba3ddcb31e5f2009ae1863d7d5b6e2c217c
Taeyoon Kim <bbung9704@yonsei.ac.kr>
2021-01-11 17:41:56 +0900

    Header alert 추가 (mobile 모드)

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -4,6 +4,8 @@ import { useHistory } from "react-router-dom";
 import "./layout.css";
 import Avatar from "@material-ui/core/Avatar";
 
+import AlertFeed from "../Main/AlertFeed.js";
+
 import { userContext } from "../../context/userContext.js";
 import { logout } from "../../api/user.js";
 
@@ -16,6 +18,7 @@ const Header = () => {
   const logoRef = useRef(false);
   const barRef = useRef(false);
   const loadRef = useRef(false);
+  const alertRef = useRef(false);
 
   const doLogout = () => {
     loadRef.current.classList.toggle("active");
@@ -30,13 +33,27 @@ const Header = () => {
     slideRef.current.classList.toggle("active");
     logoRef.current.classList.toggle("active");
   };
+
+  const alertToggle = () => {
+    alertRef.current.classList.toggle("active");
+  };
+
   return (
     <>
       <div className="sticky-top" ref={barRef}>
         <h1 id="logo" ref={logoRef}>
           Project#3
         </h1>
-        <div id="head-avatar" style={{ position: "absolute", right: "1.5rem" }}>
+        <div id="head-avatar">
+          <div className="head-alert-container" onClick={alertToggle}>
+            <div className="material-icons" id="head-alert">
+              notifications
+            </div>
+            <div id="head-alert-toggle" ref={alertRef}>
+              <AlertFeed token={userstate.userstate.token} />
+            </div>
+          </div>
+
           <Avatar
             alt={userstate.userstate.username}
             src={userstate.userstate.image}
